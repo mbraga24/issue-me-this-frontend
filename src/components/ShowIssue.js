415dc9c04648fdd644f6c3faa97db5ebe01bf996
github username <rdg.brg@gmail.com>
2020-10-11 21:05:18 -0400

    ShowIssue component refactored

--- src/components/ShowIssue.js
+++ src/components/ShowIssue.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
@@ -14,6 +14,9 @@ const ShowIssue = props => {
   const issues = useSelector(state => state.issue.issues)
   const currentUser = useSelector(state => state.user.keyHolder)
   const currentIssue = issues.find(issue => issue.id === parseInt(issueId))
+  const [ alertHeader, setAlertHeader ] = useState("")
+  const [ alertStatus, setAlertStatus ] = useState(false)
+  const [ message, setMessage ] = useState([])
 
   // const [ issue, setIssue ] = useState(null)
   const [ fields, handleFieldChange ] = useFormFields({
@@ -23,9 +26,6 @@ const ShowIssue = props => {
 
   const postComment = event => {
     event.preventDefault()
-    // removing the synthetic event from the pool allowing the event properties
-    // to be accessed in an asynchronous way
-    // event.persist()
     
     const newComent = {
       title: fields.title,
@@ -45,9 +45,10 @@ const ShowIssue = props => {
     })
     .then(r => r.json())
     .then(data => {
-      if (data.error) {
+      // console.log("POST COMMENT --->", data)
+      if (data.errorStatus) {
         console.log("POST COMMENT ERROR -->", data)
-        // handleMessages(data)
+        handleMessages(data)
       } else {
         console.log("POST COMMENT -->", data)
         dispatch({ type: ADD_COMMENT, payload: data.comment })
@@ -67,63 +68,94 @@ const ShowIssue = props => {
     ))
   }
 
-    // const { title, issue_body, user } = currentIssue && 
-    // const imgUrl = `https://semantic-ui.com/images/avatar/small/${user.avatar}.jpg`
-    
-    return (
-      currentIssue ?
-        <div className="ui container" >
-          <h1 className="ui center aligned header">Issue</h1>
-          <div className="ui card fluid">
-            <div className="content">
-              <div className="header">
-                {currentIssue.title}
-              <Link to={`/users/${currentIssue.user.id}`}>
-                <img src={`https://semantic-ui.com/images/avatar/small/${currentIssue.user.avatar}.jpg`} alt={currentIssue.user.first_name} className="ui mini right floated image" />
-              </Link>
-              </div>
-            </div>
-            <div className="content">
-              <div className="description">
-                {currentIssue.issue_body}
-              </div>
-              <Link to={`/users/${currentIssue.user.id}`}>
-                <p className="ui right aligned">- {currentIssue.user.first_name}</p>
-              </Link>
-            </div>
-          </div>
+  const handleMessages = data => {
+    console.log("HANDLE MESSAGES DATA -->", data)
+    setAlertHeader(data.header)
+    setAlertStatus(true)
+    handleDismissCountDown()
+    setMessage(data.error)
+  }
 
-          <div className="ui comments">
-            <h3 className="ui dividing header">Comments</h3>
-            {renderComments()}
-            { 
-              currentUser ? 
-              <form className="ui reply form" onSubmit={postComment}>
-                <div className="field">
-                  <input placeholder="Title" name="title" onChange={handleFieldChange}/>
-                </div>
-                <div className="field">
-                  <textarea name="comment" rows="3" onChange={handleFieldChange}></textarea>
-                </div>
-                <button type="submit" className="ui icon secondary left labeled button">
-                  <i aria-hidden="true" className="edit icon"></i>
-                  Post Comment
-                </button>
-              </form>
-              : 
-                <Button.Group size='large'>
-                  <Link to="/login">
-                    <Button color="green">Login</Button>
-                  </Link>
-                  <Button.Or />
-                  <Link to="/signup">
-                    <Button color="blue">Sign up</Button>
-                  </Link>
-                </Button.Group>
-            }
+  const renderAlertMessage = () => {
+    return message.map(message => <li className="content">{message}</li> )
+  }
+
+  const handleDismissOnClick = () => {
+    setAlertStatus(false)
+  }
+
+  const handleDismissCountDown = () => {
+    setTimeout(() => {
+      setAlertStatus(false)
+    }, 4000)
+  }
+    
+  return (
+    currentIssue ?
+      <div className="ui container" >
+        <h1 className="ui center aligned header">Issue</h1>
+        <div className="ui card fluid">
+          <div className="content">
+            <div className="header">
+              {currentIssue.title}
+            <Link to={`/users/${currentIssue.user.id}`}>
+              <img src={`https://semantic-ui.com/images/avatar/small/${currentIssue.user.avatar}.jpg`} alt={currentIssue.user.first_name} className="ui mini right floated image" />
+            </Link>
+            </div>
           </div>
-        </div> : null
-    );
+          <div className="content">
+            <div className="description">
+              {currentIssue.issue_body}
+            </div>
+            <Link to={`/users/${currentIssue.user.id}`}>
+              <p className="ui right aligned">- {currentIssue.user.first_name}</p>
+            </Link>
+          </div>
+        </div>
+
+        <div className="ui comments">
+          <h3 className="ui dividing header">Comments</h3>
+          {renderComments()}
+          { 
+            currentUser ? 
+            <form className="ui reply form" onSubmit={postComment}>
+              <div className="field">
+                <input placeholder="Title" name="title" onChange={handleFieldChange}/>
+              </div>
+              <div className="field">
+                <textarea placeholder="Text" name="comment" rows="3" onChange={handleFieldChange}></textarea>
+              </div>
+              <button type="submit" className="ui icon secondary left labeled button">
+                <i aria-hidden="true" className="edit icon"></i>
+                Post Comment
+              </button>
+              {
+                (alertStatus && !!message) && 
+                  <div className="ui negative message">
+                    <i className="close icon" onClick={handleDismissOnClick}></i>
+                    <div className="header">
+                      {alertHeader}
+                    </div>
+                    <ul className="list">
+                      {message.length !== 0 ? renderAlertMessage() : null}
+                    </ul>
+                  </div>
+              }
+            </form>
+            : 
+              <Button.Group size='large'>
+                <Link to="/login">
+                  <Button color="green">Login</Button>
+                </Link>
+                <Button.Or />
+                <Link to="/signup">
+                  <Button color="blue">Sign up</Button>
+                </Link>
+              </Button.Group>
+          }
+        </div>
+      </div> : null
+  );
 }
 
 export default withRouter(ShowIssue);
