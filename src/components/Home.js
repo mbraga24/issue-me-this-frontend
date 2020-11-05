45925cecd714ac024fe64d1f71b84e767b8767b4
github username <rdg.brg@gmail.com>
2020-11-05 12:08:07 -0500

    Color change

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ import '../resources/Home.css';
 const Home = props => {
   return (
       <div id="Home-Container">
-        <Header icon color="grey" className="Home-Header">
+        <Header icon className="Home-Header">
           <Icon name='question circle outline' className="Home-Icon"/>
-          <Header color="grey" className="Home-Title">Issue Me This</Header>
+          <Header className="Home-Title">Issue Me This</Header>
         </Header>
       </div>
   )
