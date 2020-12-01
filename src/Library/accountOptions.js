eb759e8cb9dd2dbf60793cfdc4e7dfb35fc5988b
github username <rdg.brg@gmail.com>
2020-11-30 22:17:01 -0500

    Fix accountOptions

--- src/Library/accountOptions.js
+++ src/Library/accountOptions.js
@@ -1,23 +1,19 @@
 const accountOptions = [
   {
-     color: "teal",
      iconName: "settings",
      listHeader: "Update account",
      link: "/update/account"
   },
   {
-     color: "teal",
      iconName: "star",
      listHeader: "Favorite issues",
      link: "/favorite-issues"
   },
   {
-     color: "teal",
      iconName: "boxes",
      listHeader: "Manage issues"
   },
   {
-    color: "teal",
     iconName: "thumbs up",
     listHeader: "Issues you liked",
     link: "/liked-issues"
