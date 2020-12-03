4797bc9af417bc7bd4153b952b368076b048dd95
github username <rdg.brg@gmail.com>
2020-12-02 23:51:02 -0500

    Fix card aligments

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Card, Image, Icon } from 'semantic-ui-react'
+import '../resources/UserCard.css';
 
 const UserCard = props => {
 
@@ -9,14 +10,14 @@ const UserCard = props => {
   const { id, first_name, last_name, job_title, profile_picture, issues } = props.user
 
   return (
-    <Card>
-      <Image as={Link} to={`${currentUser ? `/account/${id}` : "/login"}`} src={profile_picture.image_url} wrapped ui={true} size="medium" />
+    <Card id="UserCard-Container" className="Card-Size">
+      <Image className="Card-Image" as={Link} to={`${currentUser ? `/account/${id}` : "/login"}`} src={profile_picture ? profile_picture.image_url : "/default-profile.jpg"} wrapped ui={true} size="medium" />
       <Card.Content>
         <Link to={`${currentUser ? `/account/${id}` : "/login"}`}>
           <Card.Header>{first_name} {last_name}</Card.Header>
         </Link>
         <Card.Description>
-          Job Title: {job_title}
+          {job_title}
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
