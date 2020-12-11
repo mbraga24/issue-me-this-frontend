f80345b4b70f731ef9c70669b6a434c627d3a81f
github username <rdg.brg@gmail.com>
2020-12-11 17:08:17 -0500

    Fix artist's attribution

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,12 +13,12 @@ const Home = props => {
     <div id="Home-Container">
       <Header icon className="Home-Header">
         <Popup style={popupStyle} inverted position='top center' on="click" pinned trigger={<Image src={require('../Icon/question-mark-colored.png')} className="Image-Size" circular/>}>
-          Tab and Home Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+          Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
         </Popup>
         <Popup 
           inverted 
           position='bottom center' 
-          content={"Click the icon for the creator attribute"} 
+          content={"Click the icon for the artist's attribution."} 
           style={popupStyle} 
           trigger={<Header color="blue" className="Home-Title">Issue Me This</Header>} />
       </Header>
